89825d5416ce14038abd5b185f975d4f9688d71b
ucat <ucat.games@gmail.com>
2023-09-10 23:11:50 +0800

    net

---
+++ AutoChess/assets/script/serverSDK/common.ts
@@ -0,0 +1,190 @@
+import * as client_handle from "./client_handle";
+/*this enum code is codegen by abelkhan codegen for ts*/
+
+export enum ShopIndex{
+    Role = 0,
+    Prop = 1
+}
+
+/*this struct code is codegen by abelkhan codegen for typescript*/
+export class UserInformation
+{
+    public UserName : string = "";
+    public UserGuid : number = 0;
+
+}
+
+export function UserInformation_to_protcol(_struct:UserInformation){
+    return _struct;
+}
+
+export function protcol_to_UserInformation(_protocol:any){
+    let _struct = new UserInformation();
+    for (const [key, val] of Object.entries(_protocol)) {
+        if (key === "UserName"){
+            _struct.UserName = val as string;
+        }
+        else if (key === "UserGuid"){
+            _struct.UserGuid = val as number;
+        }
+    }
+    return _struct;
+}
+
+export class UserData
+{
+    public User : UserInformation | null = null;
+    public Strength : number = 0;
+    public RoleList : number[] = [];
+
+}
+
+export function UserData_to_protcol(_struct:UserData){
+    return _struct;
+}
+
+export function protcol_to_UserData(_protocol:any){
+    let _struct = new UserData();
+    for (const [key, val] of Object.entries(_protocol)) {
+        if (key === "User"){
+            _struct.User = protcol_to_UserInformation(val);
+        }
+        else if (key === "Strength"){
+            _struct.Strength = val as number;
+        }
+        else if (key === "RoleList"){
+            _struct.RoleList = [];
+            for(let v_ of val as any) {
+                _struct.RoleList.push(v_);
+            }
+        }
+    }
+    return _struct;
+}
+
+export class Role
+{
+    public RoleID : number = 0;
+    public Level : number = 0;
+    public BaseAttack : number = 0;
+    public BaseDefense : number = 0;
+    public TotalAttack : number = 0;
+    public TotalDefense : number = 0;
+
+}
+
+export function Role_to_protcol(_struct:Role){
+    return _struct;
+}
+
+export function protcol_to_Role(_protocol:any){
+    let _struct = new Role();
+    for (const [key, val] of Object.entries(_protocol)) {
+        if (key === "RoleID"){
+            _struct.RoleID = val as number;
+        }
+        else if (key === "Level"){
+            _struct.Level = val as number;
+        }
+        else if (key === "BaseAttack"){
+            _struct.BaseAttack = val as number;
+        }
+        else if (key === "BaseDefense"){
+            _struct.BaseDefense = val as number;
+        }
+        else if (key === "TotalAttack"){
+            _struct.TotalAttack = val as number;
+        }
+        else if (key === "TotalDefense"){
+            _struct.TotalDefense = val as number;
+        }
+    }
+    return _struct;
+}
+
+export class UserBattleData
+{
+    public User : UserInformation | null = null;
+    public coin : number = 0;
+    public RoleList : Role[] = [];
+
+}
+
+export function UserBattleData_to_protcol(_struct:UserBattleData){
+    return _struct;
+}
+
+export function protcol_to_UserBattleData(_protocol:any){
+    let _struct = new UserBattleData();
+    for (const [key, val] of Object.entries(_protocol)) {
+        if (key === "User"){
+            _struct.User = protcol_to_UserInformation(val);
+        }
+        else if (key === "coin"){
+            _struct.coin = val as number;
+        }
+        else if (key === "RoleList"){
+            _struct.RoleList = [];
+            for(let v_ of val as any) {
+                _struct.RoleList.push(protcol_to_Role(v_));
+            }
+        }
+    }
+    return _struct;
+}
+
+export class ShopData
+{
+    public SaleRoleList : number[] = [];
+    public SalePropList : number[] = [];
+
+}
+
+export function ShopData_to_protcol(_struct:ShopData){
+    return _struct;
+}
+
+export function protcol_to_ShopData(_protocol:any){
+    let _struct = new ShopData();
+    for (const [key, val] of Object.entries(_protocol)) {
+        if (key === "SaleRoleList"){
+            _struct.SaleRoleList = [];
+            for(let v_ of val as any) {
+                _struct.SaleRoleList.push(v_);
+            }
+        }
+        else if (key === "SalePropList"){
+            _struct.SalePropList = [];
+            for(let v_ of val as any) {
+                _struct.SalePropList.push(v_);
+            }
+        }
+    }
+    return _struct;
+}
+
+export class svr_info
+{
+    public tick_time : number = 0;
+    public player_num : number = 0;
+
+}
+
+export function svr_info_to_protcol(_struct:svr_info){
+    return _struct;
+}
+
+export function protcol_to_svr_info(_protocol:any){
+    let _struct = new svr_info();
+    for (const [key, val] of Object.entries(_protocol)) {
+        if (key === "tick_time"){
+            _struct.tick_time = val as number;
+        }
+        else if (key === "player_num"){
+            _struct.player_num = val as number;
+        }
+    }
+    return _struct;
+}
+
+/*this module code is codegen by abelkhan codegen for typescript*/
---
+++ AutoChess/assets/script/serverSDK/error.ts
@@ -0,0 +1,35 @@
+import * as client_handle from "./client_handle";
+/*this enum code is codegen by abelkhan codegen for ts*/
+
+export enum em_error{
+    timeout = -2,
+    db_error = -1,
+    success = 0,
+    server_busy = 1,
+    unregistered_palyer = 2,
+    login_dy_faild = 3,
+    no_enough_strength = 4
+}
+
+/*this struct code is codegen by abelkhan codegen for typescript*/
+/*this module code is codegen by abelkhan codegen for typescript*/
+export class error_code_ntf_module extends client_handle.imodule {
+    public _client_handle:client_handle.client;
+    constructor(_client_handle_:client_handle.client){
+        super();
+        this._client_handle = _client_handle_;
+        this._client_handle._modulemng.add_method("error_code_ntf_error_code", this.error_code.bind(this));
+
+        this.cb_error_code = null;
+    }
+
+    public cb_error_code : (err_code:em_error)=>void | null;
+    error_code(inArray:any[]){
+        let _argv_:any[] = [];
+        _argv_.push(inArray[0]);
+        if (this.cb_error_code){
+            this.cb_error_code.apply(null, _argv_);
+        }
+    }
+
+}
