40265c072c49874951e12361d2704b83d476c3ad
ucat <ucat.games@gmail.com>
2023-09-24 21:03:10 +0800

    game

--- AutoChess/assets/script/netDriver/netSingleton.ts
+++ AutoChess/assets/script/netDriver/netSingleton.ts
@@ -1,6 +1,8 @@
 import { SceneAsset, AssetManager } from 'cc';
 import * as player from "./netPlayer"
+import * as game from "./netGame"
 
 export class netSingleton {
     public static player = new player.netPlayer(); 
+    public static game = new game.netGame();
 }
