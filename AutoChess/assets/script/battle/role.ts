a251a9ef7295c3f6a80084e3c08c9327e0322cf9
qianqians <451517996@qq.com>
2023-09-25 11:08:44 +0800

    属性

--- AutoChess/assets/script/battle/role.ts
+++ AutoChess/assets/script/battle/role.ts
@@ -6,11 +6,16 @@
 import * as skill from './skill/skill_base'
 
 export enum Property {
-
+    HP = 1,
+    Attack = 2,
+    DamageReduction = 3,
+    DamageReductionRound = 4,
+    //
+    InevitableKill = 5, // 暂时放这里. 0 普通伤害，1 必杀
 }
 
 export class Role {
-    public skill : skill.SkillBase;
+    public skill : skill.SkillBase[]; // 一般情况只有一个技能，使用特殊食物时添加一个技能
 
     private properties : Map<Property, number>;
 
@@ -23,6 +28,9 @@ export class Role {
     }
 
     public GetProperty(em: Property) {
+        if (this.properties.has(em)) {
+            return this.properties[em];
+        }
         return 0;
     }
 }
