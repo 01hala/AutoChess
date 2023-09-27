16a43f8bc3ada4446d7706a79665954e468b37f0
ucat <ucat.games@gmail.com>
2023-09-27 22:00:03 +0800

    技能例子

--- AutoChess/assets/script/battle/skill/skill_base.ts
+++ AutoChess/assets/script/battle/skill/skill_base.ts
@@ -3,54 +3,17 @@
  * author: qianqians
  * 2023/9/24
  */
+import * as enums from '../enums'
 import * as battle from '../battle'
 
-export enum EventType {
-    Sold = 1,
-    Purchase = 2,
-    Upgrade = 3,
-    EatFood = 4,
-    RoundStarts = 5,
-    RoundEnd = 6,
-    BattleBegin = 7,
-    BeforeAttack = 8,
-    AfterAttack = 9,
-    Injured = 10,
-    Syncope = 11,
-    ChangeSelfLocation = 12,
-    ChangeEnemyLocation = 13,
-    ReleaseSkill = 14,
-}
-
-/*
- * 技能类型
- * Editor:Hotaru
- * 2023.09.25
- */
-export enum SkillType
-{
-    Intensifier = 1,
-    Attack = 2,
-    Summon = 3,
-    Support = 4,
-    Economy = 5,
-}
-
-export enum Camp {
-    None = -1,
-    Tie = 0,
-    Self = 1,
-    Enemy = 2,
-}
-
 export class RoleInfo {
-    public camp : Camp;
+    public camp : enums.Camp;
     public index : number;
     public battleCount : number;
 }
 
 export class Event {
-    public type : EventType;
+    public type : enums.EventType;
     public spellcaster : RoleInfo;
     public recipient : RoleInfo[];
     public value : number[];
