a16d8d9f702adc5489033a7a1514b2c1a4a5c988
kasuganohotaru <Moying2063@gmail.com>
2023-10-01 20:23:01 +0800

    添加tirgger：5、6、7

---
+++ AutoChess/assets/script/battle/trigger/SkillTrigger_BattleBegin_7.ts
@@ -0,0 +1,46 @@
+/*
+ * SkillTrigger_BattleBegin_7.ts
+ * author: Hotaru
+ * 2023/10/01
+ * 触发器——战斗开始
+ */
+import { Event, RoleInfo, SkillTriggerBase } from "../skill/skill_base";
+import { Camp, Direction, EventType, SkillType } from '../enums';
+
+export class SkillTrigger_BattleBegin_7 extends SkillTriggerBase
+{
+    public res:string="battle/skill/SkillTrigger_BattleBegin_7";
+
+    CheckSkillTrigger(frameEvent: Event[], selfInfo: RoleInfo): boolean 
+    {
+        try 
+        {
+            return this.CheckSkill(frameEvent,selfInfo);
+        } 
+        catch (error) 
+        {
+            console.warn(this.res+"下的 CheckSkillTrigger 错误");
+        }
+    }
+
+    private CheckSkill(frameEvent: Event[], selfInfo: RoleInfo):boolean
+    {
+        try 
+        {
+            for(let b of frameEvent)
+            {
+                if(EventType.BattleBegin==b.type)
+                {
+                    return true;
+                }
+            }
+            return false;
+        } 
+        catch (error) 
+        {
+            console.warn(this.res+"下的 CheckSkill 错误");
+        }
+    }
+    
+}
+
---
+++ AutoChess/assets/script/battle/trigger/SkillTrigger_RoundEnd_6.ts
@@ -0,0 +1,46 @@
+/*
+ * SkillTrigger_RoundEnd_6.ts
+ * author: Hotaru
+ * 2023/10/01
+ * 触发器——回合结束(准备结束)
+ */
+import { Event, RoleInfo, SkillTriggerBase } from "../skill/skill_base";
+import { Camp, Direction, EventType, SkillType } from '../enums';
+
+export class SkillTrigger_RoundEnd_6 extends SkillTriggerBase
+{
+    public res:string="battle/skill/SkillTrigger_RoundEnd_6";
+
+    CheckSkillTrigger(frameEvent: Event[], selfInfo: RoleInfo): boolean 
+    {
+        try 
+        {
+            return this.CheckSkill(frameEvent,selfInfo);
+        } 
+        catch (error) 
+        {
+            console.warn(this.res+"下的 CheckSkillTrigger 错误");
+        }
+    }
+
+    private CheckSkill(frameEvent: Event[], selfInfo: RoleInfo):boolean
+    {
+        try 
+        {
+            for(let b of frameEvent)
+            {
+                if(EventType.RoundEnd==b.type)
+                {
+                    return true;
+                }
+            }
+            return false;
+        } 
+        catch (error) 
+        {
+            console.warn(this.res+"下的 CheckSkill 错误");
+        }
+    }
+    
+}
+
