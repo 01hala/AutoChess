17c4548f2fa39905923efe3a4432e45edd6038cb
HUNZIOJ <1928268188@qq.com>
2023-10-01 08:07:39 +0800

    Update Skill_RecoveryHP_2.ts
    将回复血量类依赖角色索引修改成依赖RoleInfo

--- AutoChess/assets/script/battle/skill/Skill_RecoveryHP_2.ts
+++ AutoChess/assets/script/battle/skill/Skill_RecoveryHP_2.ts
@@ -9,39 +9,38 @@ const { ccclass, property } = _decorator;
 @ccclass('Skill_RecoveryHP_2')
 export class Skill_RecoveryHP_2 extends SkillBase {
     public res:string="battle/skill/Skill_RecoveryHP_2";
-    private effectiveIndexOfRole : number[];
+    private effectiveRoleInfo : RoleInfo[];
     private effectiveValue : number;
-    private effectiveCamp : Camp;
 
-    //回复生命脚本，第一个参数是需要回复生命值的角色索引，第二个参数是需要回复生命的数值，第三个参数是回复生命的目标阵容
-    constructor(effectiveIndexOfRole : number[], effectiveValue : number, camp : Camp)
-    {
+    constructor(effectiveRoleInfo : RoleInfo[], effectiveValue : number){
         super();
-        this.effectiveIndexOfRole = effectiveIndexOfRole;
+        this.Init(effectiveRoleInfo, effectiveValue);
+    }
+
+    //回复生命脚本，第一个参数是需要回复生命值的RoleInfo，第二个参数是需要回复生命的数值，第三个参数是回复生命的目标阵容
+    Init(effectiveRoleInfo : RoleInfo[], effectiveValue : number) : void
+    {
+        this.effectiveRoleInfo = effectiveRoleInfo;
         this.effectiveValue = effectiveValue;
-        this.effectiveCamp = camp;
     }
 
     UseSkill(selfInfo: RoleInfo, battle: Battle): void
     {
         try
         {
-            if(Camp.Enemy == this.effectiveCamp)
-                for(const currIndex of this.effectiveIndexOfRole)
-                {
-                    battle.GetEnemyTeam().GetRole(currIndex).ChangeProperties(Property.HP,this.effectiveValue);
-                }
-            else if(Camp.Self == this.effectiveCamp)
-                for(const currIndex of this.effectiveIndexOfRole)
-                {
-                    battle.GetSelfTeam().GetRole(currIndex).ChangeProperties(Property.HP,this.effectiveValue);
-                }
-            else
-                throw new error("回复血量必须明确阵容！！");
+            for(const roleInfo of this.effectiveRoleInfo)
+            {
+                if(Camp.Enemy == roleInfo.camp)
+                    battle.GetEnemyTeam().GetRole(roleInfo.index).ChangeProperties(Property.HP,this.effectiveValue);
+                else if(Camp.Self == roleInfo.camp)
+                    battle.GetSelfTeam().GetRole(roleInfo.index).ChangeProperties(Property.HP,this.effectiveValue);
+                else
+                    throw new error("阵营");
+            }
         }
         catch(e)
         {
-            console.error(this.res+"下的 UseSkill 异常");
+            console.error(this.res + "下的" +  e + "异常");
         }
     }
 }
