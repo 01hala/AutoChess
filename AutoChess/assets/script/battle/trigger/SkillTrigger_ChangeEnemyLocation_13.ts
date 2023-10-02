615e93343200b720932b67579aabf8602f7130a7
HUNZIOJ <1928268188@qq.com>
2023-10-02 21:58:04 +0800

    创建和复制粘贴和修改，晕厥、我方位置改变，敌方位置改变事件触发时类
    创建和复制粘贴和修改，晕厥、我方位置改变，敌方位置改变事件触发时类

---
+++ AutoChess/assets/script/battle/trigger/SkillTrigger_ChangeEnemyLocation_13.ts
@@ -0,0 +1,46 @@
+import { _decorator, Component, debug, log, Node, random } from 'cc';
+import { SkillBase,Event, RoleInfo,SkillTriggerBase } from '../skill/skill_base';
+import { Camp, EventType, SkillType } from '../enums';
+const { ccclass, property } = _decorator;
+
+@ccclass('SkillTrigger_ChangeEnemyLocation_13')
+export class SkillTrigger_ChangeEnemyLocation_13 extends SkillTriggerBase {
+    public res:string="battle/skill/SkillTrigger_ChangeEnemyLocation_13";
+    public EventType:EventType=EventType.ChangeEnemyLocation;
+
+    CheckSkillTrigger(frameEvent: Event[], selfInfo: RoleInfo): boolean 
+    {
+        try
+        {
+            return this.CheckSkill(frameEvent,selfInfo);          
+        }
+        catch (error) 
+        {
+            console.warn(this.res+"下的 CheckSkillTrigger 错误");
+        }
+    }
+
+    private CheckSkill(frameEvent: Event[], selfInfo: RoleInfo): boolean
+    {
+        try
+        {
+            for (let element of frameEvent) {
+                if(EventType.ChangeEnemyLocation == element.type){
+                    for (let _recipient of element.recipient) {
+                        if(_recipient.camp == selfInfo.camp && _recipient.index == selfInfo.index) {
+                            return true;
+                        }
+                    }
+                } 
+            }
+        }
+        catch (error) 
+        {
+            console.warn(this.res+"下的 CheckSkill 错误");
+        }
+
+        return false;
+    }
+}
+
+
