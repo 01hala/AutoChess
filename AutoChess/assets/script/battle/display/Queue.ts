6f02f85ca6728d3ad2008e3822f96dc565fa0803
kasuganohotaru <Moying2063@gmail.com>
2023-10-31 22:33:01 +0800

    添加召唤相关代码;修改部分函数和函数名

--- AutoChess/assets/script/battle/display/Queue.ts
+++ AutoChess/assets/script/battle/display/Queue.ts
@@ -4,7 +4,7 @@
  * 2023/10/04
  * 队列展示类
  */
-import { _decorator, Component, instantiate, Node, Prefab, resources, Vec2, Vec3 } from 'cc';
+import { _decorator, Component, instantiate, Node, Prefab, resources, tween, Vec2, Vec3 } from 'cc';
 const { ccclass, property } = _decorator;
 
 import * as RoleDis from './RoleDis'
@@ -36,37 +36,115 @@ export class Queue extends Component
     {
         
     }
-
-    async SpawnRole(r:role.Role[])
+/*
+ * 修改
+ * author：Hotaru
+ * 2023/10/31
+ * 生成角色
+ */
+    SpawnRole(r:role.Role):Promise<Node>
     {
-        try 
+        return new Promise (async (resolve)=>
         {
-            console.log(this.locationTemp);
-            console.log(r);
-
-            let address:string="Role_";
-            for(let i = 0; i<6; i++)
+            try 
             {
+                let address: string = "Role_";
                 //let roleRes=""+address+r[i].id;
                 let roleRes = address + "1";
                 let newNode = await BundleManager.Instance.loadAssetsFromBundle("Roles", roleRes) as Prefab;
-                let role=instantiate(newNode);
-                role.position=new Vec3(this.locationTemp[i].position);
+                let role = instantiate(newNode);
+
                 this.node.addChild(role);
 
-                r[i].roleNode = role;
+                r.roleNode = role;
                 this.roleNodes.push(role);
-                console.log("role:", r[i]);
+                console.log("role:", r);
 
                 let roleDis = role.getComponent(RoleDis.RoleDis);
-                await roleDis.Refresh(r[i]);
+                await roleDis.Refresh(r);
+                resolve(role);
             }
-        } 
-        catch (error) 
+            catch (error) 
+            {
+                console.warn("Queue 下的 SpawnRole 错误", error);
+                resolve(null);
+            }
+        })
+    }
+
+/*
+ * 添加
+ * author：Hotaru
+ * 2023/10/31
+ * 召唤角色
+ */
+    async SummonRole(r:role.Role[],spellcaster:role.Role)
+    {
+        try
         {
-            console.warn("Queue 下的 SpawnRole 错误", error);
+            for(let i=0;i<r.length;i++)
+            {
+                let role=await this.SpawnRole(r[i]) as Node;
+                if(null==role)
+                {
+                    console.warn("Queue 下的 SummonRole 读取的 role 为空");
+                    break;
+                }
+                let emptyIndex=-1;
+                for(let j=0;j<this.roleNodes.length;j++)
+                {
+                    if(null==this.roleNodes[j])
+                    {
+                        emptyIndex=j;
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                if(-1!=emptyIndex)
+                {
+                    role.position=spellcaster.roleNode.position;
+                    await role.getComponent(RoleDis.RoleDis).ShiftPos(this.locationTemp[emptyIndex].position);
+                }
+                else if(-1==emptyIndex)
+                {
+                    await role.getComponent(RoleDis.RoleDis).Exit();
+                }
+            }
+        }
+        catch(error)
+        {
+            console.warn("Queue 下的 SummonRole 错误", error);
         }
         
+    }
+/*
+ * 添加
+ * author：Hotaru
+ * 2023/10/31
+ * 战斗开始初始化角色
+ */
+    async InitRole(r:role.Role[])
+    {
+        try
+        {
+            for(let i = 0; i<r.length; i++)
+            {
+                let role=await this.SpawnRole(r[i]) as Node;
+                if(null==role)
+                {
+                    console.warn("Queue 下的 InitRole 读取的 role 为空");
+                    break;
+                }
+                role.position=new Vec3(this.locationTemp[i].position);
+            }
+        }
+        catch(error)
+        {
+            console.warn("Queue 下的 InitRole 错误", error);
+        }
     }
 
     async Shiftdis(r:role.Role[])
@@ -92,6 +170,8 @@ export class Queue extends Component
         {
             console.log("RemoveRole role:", role);
             let node = this.roleNodes[index];
+            this.roleNodes[index]=null;
+
             await node.getComponent(RoleDis.RoleDis).Exit();
         }
         catch (err)
