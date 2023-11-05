7bb5fe77617cf0367c67ee5ee911cd3fdbba032a
吴铭 <451517996@qq.com>
2023-11-05 16:09:03 +0800

    Shop_Skill

--- server/server/config/config.cs
+++ server/server/config/config.cs
@@ -3,15 +3,34 @@ using System.IO;
 
 namespace config
 {
+    public enum Priority
+    {
+        Low = 1,
+        Normal = 2,
+        Hight = 3,
+    }
+
+    public enum Direction
+    {
+        None = 0,
+        Forward = 1,
+        Back = 2,
+        Rigiht = 3,
+        Left = 4,
+        Self = 5,
+    }
+
     public static class Config
     {
         public static Dictionary<int, RoleConfig> RoleConfigs;
         public static Dictionary<int, BufferConfig> BufferConfigs;
+        public static Dictionary<int, ShopSkillConfig> ShopSkillConfigs;
 
         public static void Load(string path)
         {
             RoleConfigs = RoleConfig.Load(Path.Combine(path, "Role.json"));
             BufferConfigs = BufferConfig.Load(Path.Combine(path, "buffer.json"));
+            ShopSkillConfigs = ShopSkillConfig.Load(Path.Combine(path, "Shop_Skill.json"));
         }
     }
 }
---
+++ server/server/config/shop_skill.cs
@@ -0,0 +1,69 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+
+namespace config
+{
+    public class ShopSkillConfig
+    {
+        public int Id;
+        public string Name;
+        public string Type;
+        public Priority Priority;
+        public int EffectTime;
+        public int Effect;
+        public int ObjCount;
+        public Direction ObjectDirection;
+        public int Level1Value_1;
+        public int Level1Value_2;
+        public int Level2Value_1;
+        public int Level2Value_2;
+        public int Level3Value_1;
+        public int Level3Value_2;
+
+        public static Dictionary<int, ShopSkillConfig> Load(string path)
+        {
+            var obj = new Dictionary<int, ShopSkillConfig>();
+
+            FileStream fs = File.OpenRead(path);
+            byte[] data = new byte[fs.Length];
+            int offset = 0;
+            int remaining = data.Length;
+            while (remaining > 0)
+            {
+
+                int read = fs.Read(data, offset, remaining);
+                if (read <= 0)
+                    throw new EndOfStreamException("file read at" + read.ToString() + " failed");
+
+                remaining -= read;
+                offset += read;
+            }
+
+            var handle = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(System.Text.Encoding.Default.GetString(data));
+            foreach (var o in handle.Values)
+            {
+                var skillc = new ShopSkillConfig();
+                skillc.Id = (int)o["Id"];
+                skillc.Name = (string)o["Name"];
+                skillc.Type = (string)o["Type"];
+                skillc.Priority = (Priority)o["Priority"];
+                skillc.EffectTime = (int)o["EffectTime"];
+                skillc.Effect = (int)o["Effect"];
+                skillc.ObjCount = (int)o["ObjCount"];
+                skillc.ObjectDirection = (Direction)o["ObjectDirection"];
+                skillc.Level1Value_1 = (int)o["Level1Value_1"];
+                skillc.Level1Value_2 = (int)o["Level1Value_2"];
+                skillc.Level2Value_1 = (int)o["Level2Value_1"];
+                skillc.Level2Value_2 = (int)o["Level2Value_2"];
+                skillc.Level3Value_1 = (int)o["Level3Value_1"];
+                skillc.Level3Value_2 = (int)o["Level3Value_2"];
+
+                obj[skillc.Id] = skillc;
+            }
+
+            return obj;
+        }
+    }
+}
