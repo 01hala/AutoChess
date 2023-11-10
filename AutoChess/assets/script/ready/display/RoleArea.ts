bf6652bce707c13e66277139e86453c9f4e6e7f5
kasuganohotaru <Moying2063@gmail.com>
2023-11-11 02:55:23 +0800

    修改拖拽效果，解决多个角色重叠问题

--- AutoChess/assets/script/ready/display/RoleArea.ts
+++ AutoChess/assets/script/ready/display/RoleArea.ts
@@ -4,19 +4,28 @@ const { ccclass, property } = _decorator;
 @ccclass('RoleArea')
 export class RoleArea extends Component 
 {
-    public targets:Node[]=[];
+    public targets:Map<string,string> = new Map();
 
     onLoad() 
     {
         for(let t of this.node.getChildByName("Node").children)
         {
-            this.targets.push(t);
+            this.targets.set(t.name,null);
         }
     }
 
     update(deltaTime: number) 
     {
         
+    }
+
+    GetTargetValue(name:string)
+    {
+        if(this.targets.has(name))
+        {
+            return this.targets.get(name);
+        }
+        return null;
     }
 }
 
---
+++ AutoChess/assets/script/ready/display/RoleIcon.ts
@@ -0,0 +1,219 @@
+/*
+ * RoleIcon.ts
+ * author: Hotaru
+ * 2023/11/09
+ * 图标拖拽类
+ */
+
+import { _decorator, Collider, Collider2D, Component, Contact2DType, director, EventTouch, Input, instantiate, IPhysics2DContact, ITriggerEvent, Layers, Node, Prefab, rect, tween, Tween, UITransform, Vec2, Vec3, view } from 'cc';
+import { RoleArea } from './RoleArea';
+import { BundleManager } from '../../bundle/BundleManager';
+import { sleep } from '../../other/sleep';
+import * as role from '../../battle/role'
+import { RoleDis } from '../../battle/display/RoleDis';
+const { ccclass, property } = _decorator;
+
+@ccclass('RoleIcon')
+export class RoleIcon extends Component 
+{
+    @property(Node)
+    public myTouch: Node;
+
+    //public canvas:Node;
+
+    private panel:Node;
+
+    private collider:Collider2D;
+    private touchStartPoint: Vec2 = new Vec2(0, 0);
+
+    public target:Node;
+    public id:number;
+
+    private roleArea:RoleArea;
+    private rolePrefab:Prefab;
+    private roleNode:Node;
+    private iconMask:Node;
+
+    private originalPos:Vec3;
+    private tweenNode:Tween<Node>;
+
+    private isBuy:boolean=false;
+
+    protected async onLoad()
+    {
+       try
+       {
+            //this.canvas=director.getScene().getChildByPath("Canvas");
+            this.roleArea=this.node.parent.getChildByPath("RoleArea").getComponent(RoleArea);
+            this.panel=this.node.parent;
+            this.originalPos=this.node.getPosition();
+            this.iconMask=this.node.getChildByName("IconMask");
+            this.collider=this.node.getComponent(Collider2D);
+       }
+       catch(error)
+       {
+            console.error("RoleIconTouch 下的 onLoad 错误",error);
+       }
+        
+    }
+
+    async start() 
+    {
+        //this.roleNode=this.SpawnRole()
+        await this.LoadPrefab();
+
+        this.collider.on(Contact2DType.BEGIN_CONTACT, (selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null)=>
+        {
+            if(null!=otherCollider && 1 == otherCollider.group)
+            {
+                for(let i=0;i<this.roleArea.targets.size;i++)
+                {
+                    if(null==this.roleArea.GetTargetValue(otherCollider.node.name))
+                    {
+                        this.target=otherCollider.node;
+                        this.roleArea.targets.set(otherCollider.node.name,selfCollider.node.name);
+                        //console.log(otherCollider.node.name,this.roleArea.targets.get(otherCollider.node.name));
+                        return;
+                    }
+                }
+
+            }  
+        }, this);
+
+        this.collider.on(Contact2DType.END_CONTACT,(selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null)=>
+        {
+            if(null!=otherCollider && 1 == otherCollider.group)
+            {
+                for(let i=0;i<this.roleArea.targets.size;i++)
+                {
+                    if(this.roleArea.GetTargetValue(otherCollider.node.name)===selfCollider.node.name)
+                    {
+                        this.roleArea.targets.set(otherCollider.node.name,null);
+                        //console.log(otherCollider.node.name,this.roleArea.targets.get(otherCollider.node.name));
+                        return;
+                    }
+                }
+            }
+
+
+        },this)
+
+        this.myTouch.on(Input.EventType.TOUCH_CANCEL, () => 
+        {
+            
+            this.touchStartPoint = new Vec2(0, 0);
+        }, this)
+        this.myTouch.on(Input.EventType.TOUCH_END, () => 
+        {
+            this.touchStartPoint = new Vec2(0, 0);
+            this.Adsorption();
+
+        }, this)
+        this.myTouch.on(Input.EventType.TOUCH_MOVE, (event: EventTouch) => 
+        {
+
+            let node: Node = event.currentTarget;
+            
+            let pos = new Vec2();
+            let shit = pos.set(event.getUILocation());
+            let x = shit.x - view.getVisibleSize().width / 2 - this.touchStartPoint.x;
+            let y = shit.y - view.getVisibleSize().height / 2 - this.touchStartPoint.y;
+
+            node.setPosition(x, y, 0);
+
+        }, this)
+        this.myTouch.on(Input.EventType.TOUCH_START, (event: EventTouch) => 
+        {
+
+            let node: Node = event.currentTarget;
+
+            this.touchStartPoint.set(event.getUILocation());
+
+            let x = this.touchStartPoint.x - view.getVisibleSize().width / 2 - node.getPosition().x;
+            let y = this.touchStartPoint.y - view.getVisibleSize().height / 2 - node.getPosition().y;
+            
+            this.touchStartPoint = new Vec2(x, y);
+
+            this.roleNode.active=true;
+            this.iconMask.active=false;
+
+        }, this)
+    }
+
+    update(deltaTime: number) 
+    {
+
+    }
+
+    SpawnRole(r:role.Role):Promise<Node>
+    {
+        return new Promise (async (resolve)=>
+        {
+            let address: string = "Role_";
+            //let roleRes=""+address+r[i].id;
+            let roleRes = address + "1";
+            let newNode = await BundleManager.Instance.loadAssetsFromBundle("Roles", roleRes) as Prefab;
+
+            let role = instantiate(newNode);
+            let roleDis = role.getComponent(RoleDis);
+            await roleDis.Refresh(r);
+            resolve(role);
+        });
+    }
+    
+
+    async LoadPrefab()
+    {
+        let address: string = "Role_";
+        //let roleRes=""+address+r[i].id;
+        let roleRes = address + "1";
+        this.rolePrefab=await BundleManager.Instance.loadAssetsFromBundle("Roles", roleRes) as Prefab;
+        this.roleNode=instantiate(this.rolePrefab);
+        this.roleNode.setParent(this.node);
+        this.roleNode.active=false;
+    }
+
+
+    Adsorption()
+    {
+           
+        if(null!=this.target)
+        {
+
+            this.tweenNode=tween(this.node).to(0.1,{worldPosition:this.target.worldPosition})
+             .call(()=>
+             {
+            
+             })
+             .start();
+            //this.node.setWorldPosition(this.target.worldPosition);
+            this.isBuy=true;
+        }
+        else
+        {
+            this.node.setPosition(this.originalPos);
+            if(!this.isBuy)
+            {
+                this.roleNode.active=false;
+                this.iconMask.active=true;
+            }
+        }
+        
+    }
+
+    // GetUiPos(node:Node):Vec3
+    // {
+    //     try
+    //     {
+    //         let pos=this.canvas.getComponent(UITransform).convertToNodeSpaceAR(node.getWorldPosition());
+    //         return pos;
+    //     }
+    //     catch(error)
+    //     {
+    //         console.error("RoleIconTouch 下的 GetUiPos 错误",error);
+    //     }
+        
+    // }
+}
+
+
