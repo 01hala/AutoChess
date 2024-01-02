5b59dcab689ae804c33d665ce4452efecd85eb74
GuanLiu <2249479635@qq.com>
2024-01-02 12:35:11 +0800

    合并

--- AutoChess/assets/script/ready/display/PropIcon.ts
+++ AutoChess/assets/script/ready/display/PropIcon.ts
@@ -85,8 +85,21 @@ export class PropIcon extends Component
                 this.effect = jconfig.Effect;
                 this.hpBonus = jconfig.HpBonus;
                 this.attackBonus = jconfig.AttackBonus;
+                //这句从下面移到if里来了，因为还要写装备的逻辑，但是没仔细看不知道会不会出问题，所以写个注释标记一下
+                this.iconMask.getChildByPath("FoodSprite").getComponent(Sprite).spriteFrame = await this.LoadImg("battle_icon_", _id);
             }
-            this.iconMask.getChildByPath("FoodSprite").getComponent(Sprite).spriteFrame = await this.LoadImg("battle_icon_", _id);
+            else if(_type == PropsType.Equip)
+            {
+                //等装备config载入逻辑写好后这里要改
+                jconfig = config.FoodConfig.get(_id);
+                this.effect = jconfig.Effect;
+                this.hpBonus = jconfig.HpBonus;
+                this.attackBonus = jconfig.AttackBonus;
+                this.vaule=jconfig.value;
+                //差一个载入图标的逻辑，因为还不清楚装备区域
+                this.iconMask.getChildByPath("FoodSprite").getComponent(Sprite).spriteFrame = await this.LoadImg("battle_icon_", _id);
+            }
+
             this.freezeLock = _freeze;
             this.freezeSprite.active = _freeze;
 
