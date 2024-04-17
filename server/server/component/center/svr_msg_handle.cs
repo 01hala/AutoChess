257ea951adb6ecae75793a0b536e10cb379591e0
qianqians <451517996@qq.com>
2024-04-17 19:27:22 +0800

    center try catch

--- server/server/component/center/svr_msg_handle.cs
+++ server/server/component/center/svr_msg_handle.cs
@@ -29,77 +29,112 @@ namespace Abelkhan
         private void reg_server_mq(string type, string hub_type, string svr_name)
         {
             var rsp = (Abelkhan.center_reg_server_mq_rsp)_center_module.rsp.Value;
-            rsp.rsp();
 
-            _svrmng.for_each_hub((HubProxy _proxy) => {
-                _proxy.distribute_server_mq(type, svr_name);
-            });
-
-            if (type == "hub")
+            try
             {
-                var _hubproxy = _svrmng.reg_hub(_center_module.current_ch.Value, hub_type, svr_name);
+                rsp.rsp();
 
-                _svrmng.for_each_svr((SvrProxy _proxy) => {
-                    _hubproxy.distribute_server_mq(_proxy.type, _proxy.name);
+                _svrmng.for_each_hub((HubProxy _proxy) =>
+                {
+                    _proxy.distribute_server_mq(type, svr_name);
                 });
-            }
 
-            _svrmng.reg_svr(_center_module.current_ch.Value, type, hub_type, svr_name);
-        }
+                if (type == "hub")
+                {
+                    var _hubproxy = _svrmng.reg_hub(_center_module.current_ch.Value, hub_type, svr_name);
+
+                    _svrmng.for_each_svr((SvrProxy _proxy) =>
+                    {
+                        _hubproxy.distribute_server_mq(_proxy.type, _proxy.name);
+                    });
+                }
+
+                _svrmng.reg_svr(_center_module.current_ch.Value, type, hub_type, svr_name);
+            }
+            catch (Exception ex)
+            {
+                Log.Log.err("reg_server_mq:{0}", ex);
+            }
+}
 
         private void on_reconn_reg_server_mq(string type, string hub_type, string svr_name)
         {
             var rsp = (Abelkhan.center_reconn_reg_server_mq_rsp)_center_module.rsp.Value;
-            rsp.rsp();
 
-            _svrmng.for_each_new_hub((HubProxy _proxy) => {
-                _proxy.distribute_server_mq(type, svr_name);
-            });
-
-            if (type == "hub")
+            try
             {
-                var _hubproxy = _svrmng.reg_hub(_center_module.current_ch.Value, hub_type, svr_name, true);
+                rsp.rsp();
 
-                _svrmng.for_each_new_svr((SvrProxy _proxy) => {
-                    _hubproxy.distribute_server_mq(_proxy.type, _proxy.name);
+                _svrmng.for_each_new_hub((HubProxy _proxy) =>
+                {
+                    _proxy.distribute_server_mq(type, svr_name);
                 });
+
+                if (type == "hub")
+                {
+                    var _hubproxy = _svrmng.reg_hub(_center_module.current_ch.Value, hub_type, svr_name, true);
+
+                    _svrmng.for_each_new_svr((SvrProxy _proxy) =>
+                    {
+                        _hubproxy.distribute_server_mq(_proxy.type, _proxy.name);
+                    });
+                }
+                _svrmng.reg_svr(_center_module.current_ch.Value, type, hub_type, svr_name, true);
             }
-            _svrmng.reg_svr(_center_module.current_ch.Value, type, hub_type, svr_name, true);
+            catch (Exception ex)
+            {
+                Log.Log.err("on_reconn_reg_server_mq:{0}", ex);
+            }
         }
 
         private void heartbeat(uint tick)
         {
             var rsp = (Abelkhan.center_heartbeat_rsp)_center_module.rsp.Value;
-            rsp.rsp();
 
-            if (_svrmng.get_svr(_center_module.current_ch.Value, out SvrProxy _svr_proxy))
+            try
             {
-                _svr_proxy.timetmp = Service.Timerservice.Tick;
-                _svr_proxy.tick = tick;
+                rsp.rsp();
+
+                if (_svrmng.get_svr(_center_module.current_ch.Value, out SvrProxy _svr_proxy))
+                {
+                    _svr_proxy.timetmp = Service.Timerservice.Tick;
+                    _svr_proxy.tick = tick;
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Log.err("heartbeat:{0}", ex);
             }
         }
 
         private void closed()
         {
-            if (_svrmng.get_svr(_center_module.current_ch.Value, out SvrProxy _svr_proxy))
+            try
             {
-                _svr_proxy.is_closed = true;
-                _svr_proxy.closed_svr();
-            }
-
-            if (_svrmng.get_hub(_center_module.current_ch.Value, out HubProxy _hub_proxy))
-            {
-                _hub_proxy.is_closed = true;
-            }
-
-            if (_closehandle.is_closing && _svrmng.check_all_hub_closed())
-            {
-                _svrmng.close_db();
-
-                if (_svrmng.check_all_db_closed())
+                if (_svrmng.get_svr(_center_module.current_ch.Value, out SvrProxy _svr_proxy))
                 {
-                    _closehandle.is_close = true;
+                    _svr_proxy.is_closed = true;
+                    _svr_proxy.closed_svr();
                 }
+
+                if (_svrmng.get_hub(_center_module.current_ch.Value, out HubProxy _hub_proxy))
+                {
+                    _hub_proxy.is_closed = true;
+                }
+
+                if (_closehandle.is_closing && _svrmng.check_all_hub_closed())
+                {
+                    _svrmng.close_db();
+
+                    if (_svrmng.check_all_db_closed())
+                    {
+                        _closehandle.is_close = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Log.err("closed:{0}", ex);
             }
         }
     }
