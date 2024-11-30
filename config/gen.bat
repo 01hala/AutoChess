16f00e2f142ffbc8a1c2201ad27dab65daf42c67
吴铭 <451517996@qq.com>
2024-11-30 21:53:09 +0800

    SpEffect

--- config/gen.bat
+++ config/gen.bat
@@ -1,6 +1,7 @@
 cd ../meter/
 python meter.py ../config/config.xls ../config/
 python meter.py ../config/PveConfig.xls ../config/
+python meter.py ../config/SpEffect.xls ../config/
 
 cd ../config
 pause
