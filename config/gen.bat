977132779f2aca718109dde4a6095ffe6b8ba5c3
吴铭 <451517996@qq.com>
2024-12-31 23:01:51 +0800

    language

--- config/gen.bat
+++ config/gen.bat
@@ -2,6 +2,7 @@ cd ../meter/
 python meter.py ../config/config.xls ../config/json/
 python meter.py ../config/PveConfig.xls ../config/json/
 python meter.py ../config/SpEffect.xls ../config/json/
+python meter.py ../config/language.xls ../config/json/
 
 cd ../config
 pause
