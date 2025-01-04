b11f395d36c64c64f8b20c220bcbd3ae1c0f0d03
吴铭 <451517996@qq.com>
2025-01-04 13:17:35 +0800

    fixed bug ChooseTag let propc=config.EquipConfig.get(pvec.RoleEquip); propc is null

--- AutoChess/assets/script/secondaryPanel/ChooseTag.ts
+++ AutoChess/assets/script/secondaryPanel/ChooseTag.ts
@@ -105,14 +105,16 @@ export class ChooseTag extends Component
                 }
                 
                 let propc=config.EquipConfig.get(pvec.RoleEquip);
-                img =await loadAssets.LoadImg(propc.Res);
-                if(img)
-                {
-                    this.tagnodes[i].getChildByPath("Avatar/Equip/Sprite").getComponent(Sprite).spriteFrame = img;
-                }
-                else
-                {
-                    this.tagnodes[i].getChildByPath("Avatar/Equip/Sprite").getComponent(Sprite).spriteFrame=null;
+                if (propc) {
+                    img =await loadAssets.LoadImg(propc.Res);
+                    if(img)
+                    {
+                        this.tagnodes[i].getChildByPath("Avatar/Equip/Sprite").getComponent(Sprite).spriteFrame = img;
+                    }
+                    else
+                    {
+                        this.tagnodes[i].getChildByPath("Avatar/Equip/Sprite").getComponent(Sprite).spriteFrame=null;
+                    }
                 }
             }            
         }
