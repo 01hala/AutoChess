d43a382c522c41e0dacf5e54e54386808d8c15c1
Hotaru <Moying2063@gmail.com>
2025-02-17 15:16:04 +0800

    占位用音效json

--- config/gen.bat
+++ config/gen.bat
@@ -3,6 +3,7 @@ python meter.py ../config/config.xls ../config/json/
 python meter.py ../config/PveConfig.xls ../config/json/
 python meter.py ../config/SpEffect.xls ../config/json/
 python meter.py ../config/language.xls ../config/json/
+python meter.py ../config/Sound.xls ../config/json/
 
 cd ../config
 pause
