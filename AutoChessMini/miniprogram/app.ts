30f246f46f29d0ccc58e2ec3b7c0eee1a4666c2a
wuming <451517996@qq.com>
2025-12-05 14:22:28 +0800

    删除废弃模块

--- AutoChessMini/miniprogram/app.ts
+++
@@ -1,5 +0,0 @@
-// app.ts
-App<IAppOption>({
-  globalData: {},
-  onLaunch() {},
-})
--- AutoChessMini/miniprogram/pages/index/index.ts
+++
@@ -1,8 +0,0 @@
-// index.ts
-// 获取应用实例
-const app = getApp<IAppOption>()
-const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
-
-Component({
-  data: {},
-})
